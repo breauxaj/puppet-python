aa2fae0d6ac3d9004b2d2b423e8b010d21c69a64
Adam Breaux <breauxaj@gmail.com>
2013-09-20 15:21:15 -0400

    puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,19 @@
 class python {
-  $required = $operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'python', 'python-pip', 'python-setuptools', 'python-virtualenv', 'python-virtualenv-clone', 'python-virtualenvcontext', 'python-virtualenvwrapper' ],
+  $required = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      'python',
+      'python-pip',
+      'python-setuptools',
+      'python-virtualenv',
+      'python-virtualenv-clone',
+      'python-virtualenvcontext',
+      'python-virtualenvwrapper'
+    ],
   }
 
   package { $required: ensure => latest }
 
-  $path = $operatingsystem ? {
+  $path = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => '/var/virts',
   }
 
@@ -14,29 +22,6 @@ class python {
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
-  }
-
-}
-
-define python::instance ( $user = 'deploy',
-                          $group = 'deploy' ) {
-  $path = $operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => '/var/virts',
-  }
-
-  file { "$path/$name":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '0775',
-  }
-
-  exec { deploy-python:
-    path    => '/bin:/usr/bin',
-    command => "virtualenv --no-site-packages $path/$name ; chown -R $user:$group $path/$name",
-    cwd     => $path,
-    creates => "$path/$name/bin/python",
-    require => File[ "$path/$name" ],
   }
 
 }
---
+++ manifests/instance.pp
@@ -0,0 +1,24 @@
+define python::instance (
+  $user = 'deploy',
+  $group = 'deploy'
+) {
+  $path = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => '/var/virts',
+  }
+
+  file { "${path}/${name}":
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0775',
+  }
+
+  exec { 'deploy-python':
+    path    => '/bin:/usr/bin',
+    command => "virtualenv --no-site-packages ${path}/${name} ; chown -R ${user}:${group} ${path}/${name}",
+    cwd     => $path,
+    creates => "${path}/${name}/bin/python",
+    require => File[ "${path}/${name}" ],
+  }
+
+}
